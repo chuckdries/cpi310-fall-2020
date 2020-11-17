30b5550c103c1519892643684c24a6479aec1442
Chuck Dries <chuck@chuckdries.com>
2020-11-16 17:21:46 -0700

    greeting

--- index.js
+++ index.js
@@ -6,11 +6,15 @@ const app = express();
 app.engine("handlebars", exphbs());
 app.set("view engine", "handlebars");
 
+app.use(express.urlencoded());
+
 app.get("/", (req, res) => {
-  res.render("home", {
-    name: "john"
-  });
+  res.render("home");
 });
+
+app.post('/greet', (req, res) => {
+  res.render('greet', { name: req.body.name });
+})
 
 app.listen(8080, () => {
   console.log("listening on http://localhost:8080");

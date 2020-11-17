97f5c1ecf4563926f290c99a5057613c1c70c955
Chuck Dries <chuck@chuckdries.com>
2020-11-16 17:49:20 -0700

    Switch to esm for modules

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
-const express = require("express");
-var exphbs = require("express-handlebars");
+import express from "express";
+import exphbs from "express-handlebars";
+
+import sqlite3 from "sqlite3";
+import { open } from "sqlite";
+
+const dbPromise = open({
+  filename: "data.db",
+  driver: sqlite3.Database,
+});
 
 const app = express();
+
+let messages = [];
 
 app.engine("handlebars", exphbs());
 app.set("view engine", "handlebars");
@@ -9,12 +19,13 @@ app.set("view engine", "handlebars");
 app.use(express.urlencoded());
 
 app.get("/", (req, res) => {
-  res.render("home");
+  res.render("home", { messages });
 });
 
-app.post('/greet', (req, res) => {
-  res.render('greet', { name: req.body.name });
-})
+app.post("/message", (req, res) => {
+  messages.push(req.body.message);
+  res.redirect("/");
+});
 
 app.listen(8080, () => {
   console.log("listening on http://localhost:8080");
